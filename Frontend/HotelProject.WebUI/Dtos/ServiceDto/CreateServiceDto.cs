02c6702e5704a1617b08599154bd32566456c944
emredemir <emre1demir6@gmail.com>
2023-09-05 09:20:12 +0300

    anasayfa-tasarımı-tamamlandı

--- Frontend/HotelProject.WebUI/Dtos/ServiceDto/CreateServiceDto.cs
+++ Frontend/HotelProject.WebUI/Dtos/ServiceDto/CreateServiceDto.cs
@@ -4,7 +4,7 @@ namespace HotelProject.WebUI.Dtos.ServiceDto
 {
     public class CreateServiceDto
     {
-        [Required(ErrorMessage = "Hizmet ikon libki giriniz")]
+        [Required(ErrorMessage = "Hizmet ikon linki giriniz")]
         public string ServiceIcon { get; set; }
 
         [Required(ErrorMessage = "Hizmet başlığı giriniz")]
